85fffcd2da326e56d2389a9b3ed355b127a9d1a0
Isadora Barros <isa.dora786@gmail.com>
2015-02-24 14:36:23 -0300

    Remove unused methods

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,28 +1,14 @@
 class ListsController < ApplicationController
   before_action :set_list, only: [:show, :edit, :update, :destroy]
 
-  # GET /lists
-  # GET /lists.json
   def index
     @lists = List.all
   end
 
-  # GET /lists/1
-  # GET /lists/1.json
-  def show
-  end
-
-  # GET /lists/new
   def new
     @list = List.new
   end
 
-  # GET /lists/1/edit
-  def edit
-  end
-
-  # POST /lists
-  # POST /lists.json
   def create
     @list = List.new(list_params)
 
@@ -37,8 +23,6 @@ class ListsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /lists/1
-  # PATCH/PUT /lists/1.json
   def update
     respond_to do |format|
       if @list.update(list_params)
@@ -51,8 +35,6 @@ class ListsController < ApplicationController
     end
   end
 
-  # DELETE /lists/1
-  # DELETE /lists/1.json
   def destroy
     @list.destroy
     respond_to do |format|
@@ -61,19 +43,11 @@ class ListsController < ApplicationController
     end
   end
 
-  def get_tasks
-    respond_to do |format|
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_list
       @list = List.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def list_params
       params.require(:list).permit(:name)
     end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,14 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-  # GET /tasks
-  # GET /tasks.json
   def index
     @tasks = Task.all
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
-  def show
-  end
-
-  # GET /tasks/new
   def new
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
-  def edit
-  end
-
-  # POST /tasks
-  # POST /tasks.json
   def create
     @task = Task.new(task_params)
 
@@ -37,8 +23,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
     respond_to do |format|
       if @task.update(task_params)
@@ -51,8 +35,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
     @task.destroy
     respond_to do |format|
@@ -62,12 +44,10 @@ class TasksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
       params.require(:task).permit(:description)
     end
