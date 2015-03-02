2a82abaf647a715bc67eb64a9407cea5e8a71e88
Isadora Barros <isa.dora786@gmail.com>
2015-03-02 11:17:05 -0300

    Add css and javascript.

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -15,4 +15,18 @@ RSpec.describe TasksController, type: :controller do
       expect(response.status).to eq 422 # unprocessable entity
     end
   end
+
+  describe "#destroy" do
+    let(:list) { destroy(:list) }
+
+    it "returns 200 (ok) if successful" do
+      delete :destroy, list_id: list, task: attributes_for(:task), format: :js
+      expect(response).to be_ok
+    end
+
+    it "returns 422 if invalid" do
+      delete :destroy, list_id: list, task: { description: nil }, format: :js
+      expect(response.status).to eq 422 # unprocessable entity
+    end
+  end
 end
